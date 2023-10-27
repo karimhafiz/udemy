a5b404f8b59b5bf6e728ef8e8597dae42fdc569c
karimhafiz <hafiz_2010@outlook.com>
2023-10-27 18:40:11 +0100

    everything working except for the styles for expenseDate

---
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate/ExpenseDate";
+function ExpenseItem({ expenses }) {
+  return (
+    <div>
+      {expenses.length > 0 &&
+        expenses.map((item, index) => {
+          return (
+            <div className="expense-item" key={item.id}>
+              <ExpenseDate date={item.date} />
+              <div className="expense-item__description">
+                <h2>{item.title}</h2>
+                <div className="expense-item__price"> ${item.amount}</div>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default ExpenseItem;
